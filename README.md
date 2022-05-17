e12f91701fdc51598108871ba278300c45e91733
rudaoshi <sunmingming@gmail.com>
2022-05-17 15:31:47 +0800

    remove

---
+++ README.md
@@ -0,0 +1,4 @@
+# Open Information eXpression 
+
+The repo is moved to [the official repo](github.com/baidu-research/oix).
+
--- oix/parser/ud2oia/rule/converter/ud_simplifier/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-UD simplifier: modifier the UD graph:
-
-1. merge phrases into one node
-2. alter the patterns into several common patterns
-
-The modification is to easy the mapping from UD to oia-graph
-"""
